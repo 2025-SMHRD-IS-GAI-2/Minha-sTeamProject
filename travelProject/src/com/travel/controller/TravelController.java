7ca5e451a7750e5c70af1306de082b41f018927d
smhrd <happykando@naver.com>
2025-11-10 16:47:03 +0900

    짱짱 추가

--- travelProject/src/com/travel/controller/TravelController.java
+++ travelProject/src/com/travel/controller/TravelController.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface TravelController {
     List<ContinentVO> showContinents();
     List<CountryVO> showCountries(String continentId);
-    // 마라맛 아메리카노팀 화이팅!!
+    // 마라맛 아메리카노팀 화이팅!! 짱짱
     List<RestaurantVO> showRestaurants(String countryId);
     List<TouristVO> showTourists(String countryId);
     List<TravelTipVO> showTravelTips(String countryId); // 여행 Tip 메서드 추가
